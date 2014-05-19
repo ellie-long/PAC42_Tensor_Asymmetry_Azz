9dc30b2a7778a633bc070673d03ec1402c77fac8
Elena Long <ellie@jlab.org>
2014-05-19 09:23:14 -0400

    Edited clean.sh to clear azz_kinematics.tex aux files

--- clean.sh
+++ clean.sh
@@ -7,3 +7,11 @@ rm azz.dvi
 rm azz.log
 rm azz.out
 rm azz.toc
+rm azz_kinematics.aux
+rm azz_kinematics.bbl
+rm azz_kinematics.blg
+rm azz_kinematics.dvi
+rm azz_kinematics.log
+rm azz_kinematics.out
+rm azz_kinematics.toc
+rm azz_kinematics.ps
