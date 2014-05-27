b35927ced182ef554bf7bdf580a64dd647529471
Elena Long <ellie@jlab.org>
2014-05-27 10:37:03 -0400

    Trying to find out what happened to Dustin's contributions

--- clean.sh
+++ clean.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-rm azz.aux
-rm azz.bbl
-rm azz.blg
-rm azz.dvi
-rm azz.log
-rm azz.out
-rm azz.toc
-rm azz_kinematics.aux
-rm azz_kinematics.bbl
-rm azz_kinematics.blg
-rm azz_kinematics.dvi
-rm azz_kinematics.log
-rm azz_kinematics.out
-rm azz_kinematics.toc
-rm azz_kinematics.ps
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.dvi
+rm *.log
+rm *.out
+rm *.toc
+rm *.ps
